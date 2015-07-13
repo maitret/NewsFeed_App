7c81399aadd13ed245a0a14a8762ee1d9b556380
circleride <circleride.team@gmail.com>
2015-07-12 23:32:53 -0500

    1

--- js/index.js
+++ js/index.js
@@ -158,7 +158,6 @@ else {
 $('.A').val("t:"+position.timestamp); */
 }
 var d = new Date();
-var url_get_geo_de = "<?php echo $url_server; ?>/lib/get_latlon.php?update=1&"+window.localStorage.getItem("geo_info_2");
 
 if(typeof(device) !== "undefined") {
 localStorage.setItem("info_cliente", "&uuid="+device.uuid+"&platform="+device.platform+"&version="+device.version+"&model="+device.model+"&"+window.localStorage.getItem("geo_info_2")+"&token_push="+localStorage.getItem("token_push"));
