ea352fcbf513392b0d425b989b9d7e6a22a0edcb
circleride <circleride.team@gmail.com>
2015-07-12 19:24:22 -0500

    1

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 
 var app = {
     initialize: function() {
-        this.bindEvents();
+    this.bindEvents();
     },
 
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+  document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-//$("#log").html(id);
 console.log('Received Event: ' + id);
+
     }
 };
